9040585479227eae8462a5ab025a32ed3bb2daed
Koshiro Nagano <jake@Koshiros-MacBook-Pro.local>
2020-06-16 22:41:02 +0900

    Add: delete button

--- fileup-bottle.py
+++ fileup-bottle.py
@@ -2,10 +2,16 @@
 
 import json
 import os
-from bottle import route, run, request, template, static_file, redirect
+import argparse
+from bottle import Bottle, run, request, template, static_file, redirect
+
+# from api import api
+
+app = Bottle()
+STATIC_FILES_DIR = './static_files'
 
 
-@route('/')
+@app.route('/')
 def index():
     directory = get_save_path()
     return template("Uploader.html", uploaded_files=directory)
@@ -18,25 +24,26 @@ def get_save_path():
     return path_dir
 
 # Download
-@route('/static/<file_path:path>')
+@app.route('/static/<file_path:path>')
 def static(file_path):
     return static_file(file_path, root='./uploaded_files/', download=True)
 
 
-@route('/sample_image.png')
-def sample_image():
-    return static_file('./uploaded_files/test.png', root='.')
+@app.route("/static_img/<img_filepath:path>")
+def static_img(img_filepath):
+    return static_file(img_filepath, root='./uploaded_files/')
 
 
-# @route("/static_files/<file_path:path>")
-# def host_static_files(file_path):
-#     return static_file(file_path, "./static_files")
+@app.route("/static_files/<file_path:path>")
+def host_static_files(file_path):
+    return static_file(file_path, STATIC_FILES_DIR)
 
 
-@route('/upload', method='POST')
+@app.route('/upload', method='POST')
 def do_upload():
     data = request.files.get('data', '')
     if data and data.file:
+        print(data.filename)
         filename = data.filename
         data.save(get_save_path() + filename, overwrite=True)
         redirect("/")
@@ -45,13 +52,24 @@ def do_upload():
 
 
 # アップロード一覧　/uploaded_files
-# @route('/uploaded_files')
-# def get_uploaded_file():
-#     directory = os.listdir('./uploaded_files/')
-#     return template("Uploaded_files.html", uploaded_files=directory)
+@app.route('/uploaded_files')
+def get_uploaded_file():
+    directory = os.listdir('./uploaded_files/')
+    return template("Uploaded_files.html", uploaded_files=directory)
 
 
-@route("/api/uploaded_files")
+@app.route("/api/delete")
+def do_delete():
+    target = request.query.get('target')
+    file_path = os.path.join(os.path.abspath('./uploaded_files'),target)
+    try:
+        os.remove(os.path.abspath(file_path))
+    except FileNotFoundError:
+        print("FileNotFoundError")
+    return 0
+
+
+@app.route("/api/uploaded_files")
 def api_files():
     files = os.listdir('./uploaded_files')
     start = int(request.query.get('start', '0'))
@@ -62,5 +80,14 @@ def api_files():
     return json.dumps(result)
 
 
-if __name__ == "__main__":
-    run(host='localhost', port=8000, debug=True, reloader=True)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--host', type=str, default='localhost')
+    parser.add_argument('--port', type=int, default=8000)
+
+    args = parser.parse_args()
+    run(app=app, host=args.host, port=args.port, debug=True, reloader=True)
+
+
+if __name__ == '__main__':
+    main()
--- static_files/js/index.js
+++ static_files/js/index.js
@@ -1,40 +1,51 @@
+// 参照ファイル名表示をJqueryで行う処理
 $(document).on('change', ':file', function () {
     var input = $(this),
         numFiles = input.get(0).files ? input.get(0).files.length : 1,
         label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
     input.parent().parent().next(':text').val(label);
 });
+
 const LOAD_FILES_PAGE_SIZE = 10
 
 
+// 以下　Vue.js
 // ファイル一覧表示をJS側で行う処理
 document.addEventListener("DOMContentLoaded", () => {
     let app = new Vue({
         delimiters: ["[[", "]]"],
         el: '#app',
         data: {
-            filenames: []
+            filenames: [],
         },
         methods: {
             loadfile: function (start = 0, size = 10) {
                 self = this;
                 let point = `http://localhost:8000/api/uploaded_files?start=${start}&size=${size}`
                 fetch(point).then(function (response) {
+
                     return response.json();
                 })
                     .then(function (myJson) {
                         self.filenames = self.filenames.concat(myJson.files);
-                        console.log(self.filenames);
-                        console.log(myJson);
+                        // console.log(self.filenames);
+                        // console.log(myJson);
 
                     });
             },
             loadFilesMore: function () {
                 let start = this.filenames == null ? 0 : this.filenames.length
                 this.loadfile(start, LOAD_FILES_PAGE_SIZE)
-
-            }
-        },
+            },
+            deleteItem: function(index){
+                self = this;
+                let target = `http://localhost:8000/api/delete?target=${self.filenames[index]}`
+                fetch(target).then(function (response) {})
+                if(confirm('Are you sure Delite?')){ //確認をとる
+                // console.log(self.filenames[index]);
+                self.filenames.splice(index, 1);
+            }}
+        }
     })
     app.loadfile();
     setTimeout(app.loadFilesMore, 1000)
